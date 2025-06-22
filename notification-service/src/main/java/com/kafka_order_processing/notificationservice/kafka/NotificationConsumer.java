85824636f0895fe7556dd46dc8f2f16d12bedb41
Aline Lima <alinelmcoelho@gmail.com>
2025-06-22 14:04:14 -0300

    Implementação do envio de notificações

---
+++ notification-service/src/main/java/com/kafka_order_processing/notificationservice/kafka/NotificationConsumer.java
@@ -0,0 +1,25 @@
+package com.kafka_order_processing.notificationservice.kafka;
+
+import com.kafka_order_processing.notificationservice.dto.InventoryEventDTO;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationConsumer {
+
+    @KafkaListener(topics = "inventory-events", groupId = "notification_group")
+    public void consumeInventoryEvent(InventoryEventDTO event) {
+        System.out.println("📬 Notificação Recebida:");
+        System.out.println("==============================================");
+        System.out.println("  Pedido ID: " + event.orderId());
+        System.out.println("  Cliente: " + event.customerName());
+        System.out.println("  Status: " + event.status());
+
+        if (event.status() == InventoryEventDTO.Status.SUCCESS) {
+            System.out.println("  Email/SMS Simulado: 'Olá " + event.customerName() + ", seu pedido " + event.orderId() + " foi confirmado e está sendo preparado!'");
+        } else {
+            System.out.println("  Email/SMS Simulado: 'Olá " + event.customerName() + ", infelizmente houve um problema com seu pedido " + event.orderId() + ". Motivo: " + event.message() + "'");
+        }
+        System.out.println("==============================================");
+    }
+}
